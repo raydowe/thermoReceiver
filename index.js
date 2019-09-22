c76378d5c7c86d10ad9c7f9f0e7f091bcef2299c
Raymond Dowe <raymond.dowe@retailinmotion.com>
2019-09-22 13:42:44 +0100

    Reloading db on each request.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //const request = require('request');
 const express = require('express');
-const db = require('better-sqlite3')('./temperature.sqlite', {});
 const app = express();
 const port = 3001;
 
 app.use(express.static('public'))
 
 app.get('/readings', (req, res) => {
-  const db = require('better-sqlite3')('./temperature.sqlite', {});
+  var db = require('better-sqlite3')('./temperature.sqlite', {});
   var readings = db.prepare('SELECT * FROM Readings').all();
 	res.send(readings);
 });
