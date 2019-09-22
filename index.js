32d071e26d41b152af4c22940fea16c267683395
Raymond Dowe <raymond.dowe@retailinmotion.com>
2019-09-22 13:39:43 +0100

    Simple chart. Clearning table of bad data.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3001;
 app.use(express.static('public'))
 
 app.get('/readings', (req, res) => {
+  const db = require('better-sqlite3')('./temperature.sqlite', {});
   var readings = db.prepare('SELECT * FROM Readings').all();
 	res.send(readings);
 });
