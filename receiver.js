b17610bc9288ca3b07ddff71e489870ca37ff359
Raymond Dowe <raydowe@gmail.com>
2019-09-22 19:29:19 +0000

    Organising weather and temperature receiver.

--- receiver.js
+++ receiver.js
@@ -1,46 +1,94 @@
 const fs = require('fs');
 const request = require('request');
-const isPi = require('detect-rpi');
-const rcswitch = (isPi()) ? require('rcswitch-gpiomem3') : null;
-
-var configuration = JSON.parse(fs.readFileSync(__dirname + '/configuration.json'));
-
-if (isPi()) {
+const is_pi = require('detect-rpi')();
+var rcswitch;
+if (is_pi) {
+  console.log('Starting listeneing for 433Mhz messages...')
+  rcswitch = require('rcswitch-gpiomem3')
   rcswitch.enableReceive(2);
-  var last_message;
-  var receive_timer = setInterval(function () {
-    if (rcswitch.available()) {
+}
+
+var Receiver = function() {
+
+  var ctx = this;
+  var configuration = JSON.parse(fs.readFileSync(__dirname + '/configuration.json'));
+  var message_timer;
+  var heartbeat_timer;
+  var sensor_values = {};
+
+  this.init = function() {
+    var heartbeat_timer = setInterval(ctx.heartbeat, 5 * 60 * 1000);
+    setTimeout(function() {
+      ctx.heartbeat();
+    }, 5000);
+
+    message_timer = setInterval(ctx.checkMessageQueue, 500);
+    ctx.checkMessageQueue();
+  }
+
+  this.heartbeat = function() {
+    if (ctx.messageNeededForSensor(1, '5 minutes')) {
+      ctx.getMessage(1);
+    }
+
+    if (ctx.messageNeededForSensor(0, '15 minutes')) {
+      ctx.getWeather();
+    }
+  }
+
+  this.messageNeededForSensor = function(sensor, duration) {
+    var db = ctx.getDatabase();
+    var results = db.prepare('SELECT * FROM Readings WHERE sensor = ? AND created >= Datetime("now", "-' + duration + '")').get(sensor);
+    return results == undefined;
+  }
+
+  this.getMessage = function(sensor) {
+    var temperature = sensor_values[sensor.toString()];
+    if (temperature == undefined) {
+      // no message queued
+      return;
+    }
+    ctx.saveReading(sensor, temperature)
+  }
+
+  this.checkMessageQueue = function() {
+    if (is_pi && rcswitch.available()) {
       var message = rcswitch.getReceivedValue().toString();
-      if (last_message != message) {
-        last_message = message;
-        var sensor = message.substring(0, 1);
-        var temperature = parseInt(message.substring(1, 5)) / 100;
-        console.log(sensor + ': ' + temperature);
-        var db = require('better-sqlite3')('./temperature.sqlite', {});
-        db.prepare('INSERT INTO Readings (sensor, temperature) VALUES(?, ?)').run(sensor, temperature);
-      }
+      var sensor = message.substring(0, 1);
+      var temperature = parseInt(message.substring(1, 5)) / 100;
+      sensor_values[sensor.toString()] = temperature;
       rcswitch.resetAvailable();
     }
-  }, 500);
-}
+  }
 
+  this.getWeather = function() {
+    console.log('Getting weather...');
+    request(
+  		{
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + configuration.openweather.lat + '&lon=' + configuration.openweather.lon + '&appid=' + configuration.openweather.api_key + '&units=metric',
+  			method:'GET'
+  		},
+  		function(error, response, body) {
+        console.log('Response received...');
+        var json = JSON.parse(body);
+        var sensor = 0;
+        var temperature = json.main.temp;
+        ctx.saveReading(sensor, temperature);
+  		}
+  	);
+  }
 
-function getWeather() {
-  request(
-		{
-      url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + configuration.openweather.lat + '&lon=' + configuration.openweather.lon + '&appid=' + configuration.openweather.api_key + '&units=metric',
-			method:'GET'
-		},
-		function(error, response, body) {
-      var json = JSON.parse(body);
-      var sensor = 0;
-      var temperature = json.main.temp;
-      console.log(sensor + ': ' + temperature);
-      var db = require('better-sqlite3')('./temperature.sqlite', {});
-      db.prepare('INSERT INTO Readings (sensor, temperature) VALUES(?, ?)').run(sensor, temperature);
-		}
-	);
-}
+  this.saveReading = function(sensor, temperature) {
+    console.log('Saving ' + sensor + ': ' + temperature);
+    var db = ctx.getDatabase();
+    db.prepare('INSERT INTO Readings (sensor, temperature) VALUES(?, ?)').run(sensor, temperature);
+  }
 
-var weather_timer = setInterval(getWeather, 1000 * 60 * 5);
-getWeather();
+  this.getDatabase = function() {
+    var db = require('better-sqlite3')('./temperature.sqlite', {});
+    return db;
+  }
+
+  ctx.init();
+
+}()
